dbf53b7b2c561b1cd89b6d03a0f0b6eae9d0ec71
Muiz Khan <muiz@replenium.com>
2019-07-23 19:15:13 +0500

    Modules imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
+import Home from "./components/home/index";
+import About from "./components/about/index";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header icon="fab fa-github" />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            {/* <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} /> */}
+            )} />
+          </Switch>
+        </div>
+        <Footer icon="fab fa-github" />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
